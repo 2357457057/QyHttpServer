714bd6b727bee0736dafa83f3130e77238f9ed0b
SeverusQy <yangyongji123@live.con>
2023-10-22 12:38:39 +0800

    Engine2

--- src/main/java/top/yqingyu/httpserver/engine2/Engine2.java
+++ src/main/java/top/yqingyu/httpserver/engine2/Engine2.java
@@ -5,11 +5,12 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import top.yqingyu.common.utils.ThreadUtil;
+import top.yqingyu.httpserver.common.ServerConfig;
 
 public class Engine2 {
     public static void start(String SERVER_NAME, int port) throws InterruptedException {
         NioEventLoopGroup SERVER = new NioEventLoopGroup(1);
-        NioEventLoopGroup CLIENT = new NioEventLoopGroup(ThreadUtil.createThFactoryC(SERVER_NAME, port + "-Handler"));
+        NioEventLoopGroup CLIENT = new NioEventLoopGroup(ServerConfig.handlerNumber, ThreadUtil.createThFactoryC(SERVER_NAME, port + "-Handler"));
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(SERVER, CLIENT);
         bootstrap.channel(NioServerSocketChannel.class);
